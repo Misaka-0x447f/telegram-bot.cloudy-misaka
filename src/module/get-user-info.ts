7a7769942bcad29f24b9d0a37f847368247457a5
MisakaKumomi <447f.misaka@outlook.com>
2022-07-03 23:16:14 +0800

    feat: galnet config

--- src/module/get-user-info.ts
+++ src/module/get-user-info.ts
@@ -2,6 +2,7 @@ import persistConfig from '../utils/configFile'
 import { getTelegramBotByAnyBotName } from '../interface/telegram'
 import * as tt from 'telegraf/typings/telegram-types'
 import errorMessages from '../utils/errorMessages'
+import { isNumeric } from '../utils/lang'
 
 const chatIdInfo = (chat: tt.Chat) =>
   [
@@ -20,32 +21,35 @@ const paramDefinition = {
   ]
 }
 
-for (const [botName, _] of Object.entries(
-  persistConfig.entries.getUserInfo
-)) {
+for (const [botName, _] of Object.entries(persistConfig.entries.getUserInfo)) {
   const bot = getTelegramBotByAnyBotName(botName)
-  bot.command.sub(async ({ ctx, args, commandName, sendMessageToCurrentChat }) => {
-    if (commandName !== 'get_user_info' || !ctx.chat) return
-    if (args[0]?.match(/^((?!\s).)+$/)) {
-      bot.sendMessage(ctx.chat.id, '正在查询').then()
-      try {
-        const chatInfo = await bot.instance.telegram.getChat(parseInt(args[0]))
-        await sendMessageToCurrentChat(chatIdInfo(chatInfo))
-      } catch (e) {
-        if (e.description === 'Bad Request: chat not found') {
-          await sendMessageToCurrentChat(
-            '会话不存在。请注意，如果我没有加入目标群或者没有和目标用户对话过，则无法查询到信息。'
+  bot.command.sub(
+    async ({ ctx, args, commandName, sendMessageToCurrentChat }) => {
+      if (commandName !== 'get_user_info' || !ctx.chat) return
+      if (args[0]?.match(/^(\d+|\w+)$/)) {
+        await sendMessageToCurrentChat('正在查询')
+        try {
+          const chatInfo = await bot.instance.telegram.getChat(
+            isNumeric(args[0]) ? parseInt(args[0]) : `@${args[0]}`
           )
-        } else {
-          await sendMessageToCurrentChat(JSON.stringify(e))
+          await sendMessageToCurrentChat(chatIdInfo(chatInfo))
+        } catch (e) {
+          if (e.description === 'Bad Request: chat not found') {
+            await sendMessageToCurrentChat(
+              '会话不存在。如要查询用户名，请用格式 @<publicName>。请注意，如果我没有加入目标群或者没有和目标用户对话过，则无法查询到信息。'
+            )
+          } else {
+            await sendMessageToCurrentChat(JSON.stringify(e))
+          }
         }
+      } else if (!args[0]) {
+        await sendMessageToCurrentChat('正在查询')
+        await sendMessageToCurrentChat(chatIdInfo(ctx.chat))
+      } else {
+        await sendMessageToCurrentChat(
+          errorMessages.illegalArguments(paramDefinition)
+        )
       }
-    } else if (!args[0]) {
-      await sendMessageToCurrentChat(chatIdInfo(ctx.chat)).then()
-    } else {
-      await sendMessageToCurrentChat(
-        errorMessages.illegalArguments(paramDefinition)
-      )
     }
-  })
+  )
 }
